b622dfe9bc6f9a5c3c5d31b2590efebfbc9d8938
NicoCardenas <nicolas.cardenas@mail.escuelaingedu.co>
2019-03-21 09:58:37 -0500

    implement carservice

--- Heroku-Spring-Lab-master/src/main/java/edu/eci/services/CarServices.java
+++ Heroku-Spring-Lab-master/src/main/java/edu/eci/services/CarServices.java
@@ -1,5 +1,42 @@
 package edu.eci.services;
 
-public class CarServices {
+import edu.eci.models.Car;
+import edu.eci.persistences.repositories.ICarRepository;
+import edu.eci.services.contracts.ICarServices;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class CarServices implements ICarServices {
+    
+    @Autowired
+    @Qualifier("CarMemoryRepository")
+    private ICarRepository carRepository;
+
+    @Override
+    public List<Car> list() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Car create(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Car get(String licence) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Car update(Car car) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Car delete(UUID id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
